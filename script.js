f70c8846ed0b6e8621ec790da98f0efee44c308e
Minhee Jeong <jungminhee0730@naver.com>
2026-04-20 06:16:47 +0900

    added style to the button and make the theme remain even if the tab changed

--- script.js
+++ script.js
@@ -4,7 +4,26 @@ document.addEventListener("DOMContentLoaded",init);
 function init(){
     let toggle = document.querySelector("#toggleButton");
     toggle.addEventListener("click",changeTheme);
+
+
+    const clickedToggle = document.querySelector("#toggleButton");
+    const body = document.querySelector("body")
+    const hd = document.querySelector("header") ;
+    const main = document.querySelector("main") ;
+    const ftr = document.querySelector("footer") ;
+
+    if(localStorage.getItem('theme') === "Night"){
+
+        clickedToggle.classList.add("nightTheme");
+        clickedToggle.textContent = "☾" ;
+        body.classList.add("nightTheme");
+        hd.classList.add("nightTheme");
+        main.classList.add("nightTheme");
+        ftr.classList.add("nightTheme");
+    }
 }
+
+
 
 function changeTheme(event){
     const clickedToggle = event.currentTarget;
@@ -13,20 +32,30 @@ function changeTheme(event){
     const main = document.querySelector("main") ;
     const ftr = document.querySelector("footer") ;
 
-    if(clickedToggle.classList.contains("nightTheme")){
+    if(localStorage.getItem('theme') === "Night"){
+        localStorage.setItem('theme', 'Day') ;
+
         clickedToggle.classList.remove("nightTheme");
+        clickedToggle.textContent = "☀" ;
         body.classList.remove("nightTheme")
         hd.classList.remove("nightTheme");
         main.classList.remove("nightTheme");
         ftr.classList.remove("nightTheme");
     }else{
+        localStorage.setItem('theme', 'Night');
+
         clickedToggle.classList.add("nightTheme");
+        clickedToggle.textContent = "☾" ;
         body.classList.add("nightTheme");
         hd.classList.add("nightTheme");
         main.classList.add("nightTheme");
         ftr.classList.add("nightTheme");
     }
 
-
 }
+
+
+
+
+
 
